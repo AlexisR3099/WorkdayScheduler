14f2e278f895865b7ede2ecda140686df77b4d8f
Slimeball <alexramo2330@gmail.com>
2022-08-15 17:27:51 -0500

    save-tasks-localstorage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 function currentTime() {
     let thisHour = parseInt(moment(new Date()).format('H'));
-    $('row').each(function() {
+    $('.row').each(function() {
         let rowHour = $(this).data('hour');
         if(thisHour > rowHour) {
             $(':nth-child(2)', this).toggleClass('past');
@@ -19,4 +19,42 @@ function currentTime() {
     })
 }
 
+$('.description').click(function() {
+    let thisTask = $('p', this);
+    let taskText = thisTask.text().trim();
+    if(taskText.length > 0) {
+        let textInput = $('<input>').addClass('form-control').val(taskText);
+        thisTask.replaceWith(textInput);
+    } else {
+        let textInput = $('<input>').addClass('form-control').val(taskText).attr('placeholder', 'Enter your tasks here!');
+        thisTask.replaceWith(textInput);
+    }
+});
+
+$('row').on('mouseleave', 'input', function() {
+    let thisTask = $(':nth-child(2) input');
+    let userTask = thisTask.val();
+    let pEelement = $('<p>').text(userTask);
+    thisTask.replaceWith(pEelement);
+});
+
+$('.saveBtn').click(function() {
+    let parentHour = $(this).parent().data('hour');
+    let thisArray = "_" + parentHour;
+    let arrayContent = $(this).siblings('div').find('p').text().trim();
+    let newArray = eval(thisArray + "=[\"" + arrayContent + "\"");
+    localStorage.setItem("\"" + thisArray + "\"", JSON.stringify(newArray));
+});
+
+function fromLocalStorage() {
+    $('.row').each(function(i) {
+        let rowHour = $(this).data('hour');
+        let newArrayName = "_" + rowHour;
+        let localStorageObj = localStorage.getItem("\"" + newArrayName + "\"");
+        let objText = JSON.parse(localStorageObj);
+        $(this).find('p').text(objText);
+    })
+};
+
 currentTime();
+fromLocalStorage();
