020f21ab7fd1791f9857357a7e85d4af0f7ee7d4
Slimeball <alexramo2330@gmail.com>
2022-08-15 16:54:53 -0500

    check-time-logic

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,3 +2,21 @@ var today = moment(new Date()).format('dddd, MMMM MM, YYYY');
 $(document).ready(function() {
     $('#currentDay').text(today)
 });
+
+function currentTime() {
+    let thisHour = parseInt(moment(new Date()).format('H'));
+    $('row').each(function() {
+        let rowHour = $(this).data('hour');
+        if(thisHour > rowHour) {
+            $(':nth-child(2)', this).toggleClass('past');
+        }
+        else if(thisHour === rowHour) {
+            $(':nth-child(2)', this).toggleClass('present');
+        }
+        else if(thisHour < rowHour) {
+            $(':nth-child(2)', this).toggleClass('future');
+        }
+    })
+}
+
+currentTime();
